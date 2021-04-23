c314a18df7bb184f6a5ba4d5de70b94bf34e63f6
Juan Orantes <carlos.oran15@hotmail.com>
2021-04-22 20:50:12 -0700

    html basic layout, urls setup, views render pages

--- group_project/artOff/urls.py
+++ group_project/artOff/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('welcome-landing', views.landing)
+    path('register', views.register_page),
+    path('login', views.login_page),
+    path('home', views.home_page),
+    path('user_profile', views.user_profile_page),
+    path('arena', views.arena_page),
+    path('gallery', views.gallery_page),
+    path('delete_confirm', views.delete_confirm_page),
 ]
--- group_project/artOff/views.py
+++ group_project/artOff/views.py
@@ -2,8 +2,34 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+# LANDING PAGE
 def index(request):
-    return HttpResponse("THIS IS THE PAGE!")
-
-def landing(request):
     return render(request, "landing.html")
+
+# REGISTER PAGE
+def register_page(request):
+    return render(request, "register.html")
+
+# LOGIN PAGE
+def login_page(request):
+    return render(request, "login.html")
+
+# HOME PAGE
+def home_page(request):
+    return render(request, "home.html")
+
+# USER PROFILE PAGE
+def user_profile_page(request):
+    return render(request, "user_profile.html")
+
+# ARENA PAGE
+def arena_page(request):
+    return render(request, "arena.html")
+
+# GALLERY PAGE 
+def gallery_page(request):
+    return render(request, "gallery.html")
+
+# DELETE PAGE
+def delete_confirm_page(request):
+    return render(request, "delete_confirm.html")
